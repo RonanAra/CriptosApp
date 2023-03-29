fb6684ef69cbee95a5356b902d27dc436c2b2473
Ronan Fernandes <ronanlm10@hotmail.com>
2023-03-29 19:42:06 -0300

    feat: Implementing hilt di

---
+++ app/src/main/java/com/example/coinbase/di/RepositoryModule.kt
@@ -0,0 +1,16 @@
+package com.example.coinbase.di
+
+import com.example.coinbase.api.HomeRepository
+import com.example.coinbase.api.HomeRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+    @Binds
+    fun bindCharacterRepository(impl: HomeRepositoryImpl): HomeRepository
+}

475f86009be8ed8cb00983d41bee41f86f3a9580
Ronan Fernandes <ronanlm10@hotmail.com>
2024-01-01 21:30:55 -0300

    chore: moved to other package

--- app/src/main/java/com/example/coinbase/presentation/home/components/CoinCardItem.kt
+++ app/src/main/java/com/example/coinbase/presentation/home/components/CoinCardItem.kt
@@ -1,4 +1,4 @@
-package com.example.coinbase.presentation.home.compose.components
+package com.example.coinbase.presentation.home.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
--- app/src/main/java/com/example/coinbase/presentation/home/components/ListCoins.kt
+++ app/src/main/java/com/example/coinbase/presentation/home/components/ListCoins.kt
@@ -1,30 +1,26 @@
-package com.example.coinbase.presentation.home.compose.components
+package com.example.coinbase.presentation.home.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.coinbase.data.models.response.CoinResponse
-import com.example.coinbase.presentation.home.HomeViewModel
 
 @Composable
 fun ListCoins(
+    listCoins: List<CoinResponse>,
     onClickItem: (CoinResponse) -> Unit,
-    modifier: Modifier = Modifier,
-    homeViewModel: HomeViewModel = viewModel()
+    modifier: Modifier = Modifier
 ) {
-    val state by homeViewModel.uiState.collectAsState()
+
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = modifier.padding(top = 4.dp)
     ) {
-        items(state.list) { item ->
+        items(listCoins) { item ->
             CoinCardItem(
                 item = item,
                 onClickItem = { onClickItem(item) }

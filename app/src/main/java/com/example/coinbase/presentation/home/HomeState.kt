45396ac87736b78be1dedf63fc220c1756129834
Ronan Fernandes <ronanlm10@hotmail.com>
2024-01-09 14:50:58 -0300

    refactor: Remove home state

--- app/src/main/java/com/example/coinbase/presentation/home/HomeState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coinbase.presentation.home
-
-sealed class HomeState {
-    data class Loading(val loading: Boolean): HomeState()
-    data class Error(val message: String): HomeState()
-}

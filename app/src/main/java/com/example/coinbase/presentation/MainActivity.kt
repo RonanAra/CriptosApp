9f1eb864eadf88a84c40b6cc5d602c1da9564e05
Ronan Fernandes <ronanlm10@hotmail.com>
2024-01-25 14:51:23 -0300

    feat: Impl splash screen

--- app/src/main/java/com/example/coinbase/presentation/MainActivity.kt
+++ app/src/main/java/com/example/coinbase/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.coinbase.presentation
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.coinbase.presentation.navigation.CoinAppNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             CoinAppNavHost()
         }

2f467b14ed855a37984396e42e44901f87d27cd8
Ronan Fernandes <ronanlm10@hotmail.com>
2024-01-30 20:07:35 -0300

    feat: Impl mapper

--- app/src/main/java/com/example/coinbase/data/repository/Repository.kt
+++ app/src/main/java/com/example/coinbase/data/repository/Repository.kt
@@ -1,16 +1,17 @@
 package com.example.coinbase.data.repository
 
-import com.example.coinbase.data.mapper.toItemCoinModel
 import com.example.coinbase.data.service.CoinService
 import com.example.coinbase.domain.entity.CoinModel
+import com.example.coinbase.domain.mapper.CoinListMapperToModel
 import com.example.coinbase.domain.repository.HomeRepository
 import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor(
     private val service: CoinService
 ): HomeRepository {
-
     override suspend fun getCoins(): List<CoinModel> {
-        return service.getCoinsList().data.map { it.toItemCoinModel() }
+        return service.getCoinsList().data.map {
+            CoinListMapperToModel().mapFrom(it)
+        }
     }
 }
---
+++ app/src/main/java/com/example/coinbase/domain/mapper/CoinListMapper.kt
@@ -0,0 +1,20 @@
+package com.example.coinbase.domain.mapper
+
+import com.example.coinbase.common.mapper.Mapper
+import com.example.coinbase.data.models.response.CoinResponse
+import com.example.coinbase.domain.entity.CoinModel
+
+class CoinListMapperToModel : Mapper<CoinResponse, CoinModel> {
+    override fun mapFrom(from: CoinResponse): CoinModel {
+        from.apply {
+            return CoinModel(
+                color = color,
+                id = id,
+                imageUrl = imageUrl,
+                name = name,
+                symbol = symbol,
+                website = website
+            )
+        }
+    }
+}

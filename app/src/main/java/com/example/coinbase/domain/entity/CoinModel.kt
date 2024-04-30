7186e291c5a5ba42ee48af8bc4cac18854e289b2
Ronan Fernandes <ronan.araujo@dimensa.com.br>
2024-04-30 14:20:22 -0300

    feat: Impl kotlin serialization

--- app/src/main/java/com/example/coinbase/domain/entity/CoinModel.kt
+++ app/src/main/java/com/example/coinbase/domain/entity/CoinModel.kt
@@ -1,12 +1,15 @@
 package com.example.coinbase.domain.entity
 
-import java.io.Serializable
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class CoinModel(
     val color: String,
     val id: String,
+    @SerialName("image_url")
     val imageUrl: String,
     val name: String,
     val symbol: String,
     val website: String,
-) : Serializable
+)

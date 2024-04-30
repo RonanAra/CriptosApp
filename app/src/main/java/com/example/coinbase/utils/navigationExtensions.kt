58ddaa56b27e1fd55c3583608b1896b19af887da
Ronan Fernandes <ronan.araujo@dimensa.com.br>
2024-04-30 14:20:03 -0300

    feat: Impl kotlin json serialization to encode object

--- app/src/main/java/com/example/coinbase/utils/navigationExtensions.kt
+++ app/src/main/java/com/example/coinbase/utils/navigationExtensions.kt
@@ -1,23 +1,22 @@
 package com.example.coinbase.utils
 
 import android.net.Uri
-import com.google.gson.Gson
-import com.google.gson.JsonSyntaxException
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
-fun <T> T.encodeObjectToArgs(): String {
+inline fun <reified T> T.encodeObjectToArgs(): String {
     return try {
-        Uri.encode(Gson().toJson(this))
-    } catch (_: RuntimeException) {
+        Uri.encode(Json.encodeToString(this))
+    } catch (e: Exception) {
+        e.printStackTrace()
         ""
     }
 }
 
-inline fun <reified R> String?.decodeObjectToArgs(): R? {
+inline fun <reified R> String.decodeObjectToArgs(): R? {
     return try {
-        if (!this.isNullOrEmpty()) {
-            Gson().fromJson(this, R::class.java)
-        } else null
-    } catch (e: JsonSyntaxException) {
+        Json.decodeFromString<R>(this)
+    } catch (e: Exception) {
         e.printStackTrace()
         null
     }

16f5b202fecaf1472ba51e553ac5343b0b776a5f
ronanlm10@hotmail.com <ronanlm10@hotmail.com>
2024-05-09 13:25:35 -0300

    refactor: Add model to args

--- app/src/main/java/com/example/coinbase/presentation/navigation/CoinAppDestinations.kt
+++ app/src/main/java/com/example/coinbase/presentation/navigation/CoinAppDestinations.kt
@@ -1,12 +1,10 @@
 package com.example.coinbase.presentation.navigation
 
+import com.example.coinbase.domain.entity.CoinModel
 import kotlinx.serialization.Serializable
 
 @Serializable
 object Home
 
 @Serializable
-data class Detail(
-    val title: String,
-    val url: String
-)
+data class Detail(val model: CoinModel)

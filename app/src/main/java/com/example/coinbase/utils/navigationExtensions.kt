476789ce70073abfebe1c646388f98a2d310152c
ronanlm10@hotmail.com <ronanlm10@hotmail.com>
2024-07-10 15:48:32 -0300

    refactor: Adjust extensions

--- app/src/main/java/com/example/coinbase/utils/navigationExtensions.kt
+++ app/src/main/java/com/example/coinbase/utils/navigationExtensions.kt
@@ -1,6 +1,8 @@
 package com.example.coinbase.utils
 
 import android.net.Uri
+import android.os.Bundle
+import androidx.navigation.NavType
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
@@ -21,5 +23,31 @@ inline fun <reified R> String.decodeObjectToArgs(): R? {
     } catch (e: Exception) {
         e.printStackTrace()
         null
+    }
+}
+
+inline fun <reified T : Any> serializableType(
+    isNullableAllowed: Boolean = false
+) = object : NavType<T>(isNullableAllowed = isNullableAllowed) {
+    override fun get(
+        bundle: Bundle,
+        key: String
+    ): T? {
+        return bundle.getString(key)?.let<String, T>(Json::decodeFromString)
+    }
+
+    override fun parseValue(value: String): T = Json.decodeFromString<T>(value)
+
+    override fun serializeAsValue(value: T): String = Json.encodeToString(value)
+
+    override fun put(
+        bundle: Bundle,
+        key: String,
+        value: T
+    ) {
+        bundle.putString(
+            key,
+            Json.encodeToString(value)
+        )
     }
 }
